78f7f2bcac5842b807bbdc885f8688330551305f
Julian033 <julian.arias035@gmail.com>
2023-04-18 15:05:42 -0600

    Mod Producción

--- frontend/src/app/services/dashboard.service.ts
+++ frontend/src/app/services/dashboard.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class DashboardService {
   
-  url = 'https://cmi.tabascoweb.com';
+  url = 'https://cmi.tabascoweb.com/';
   constructor(private httpClient:HttpClient) { }
 
 
